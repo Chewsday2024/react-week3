a8503ec8496852e28bee2a04307f3ce4763c835d
Chewsday <z647623@gmail.com>
2025-02-09 23:10:59 +0800

    react week3-2

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,10 @@ function App() {
 
 
   useEffect(() =>{
-    const token = document.cookie.replace(/(?:(?:^|.*;\s*)dogfood\s*\=\s*([^;]*).*$)|^.*$/,"$1",);
+    const token = document.cookie.replace(
+      /(?:(?:^|.*;\s*)dogfood\s*\=\s*([^;]*).*$)|^.*$/,
+      "$1",
+    );
     axios.defaults.headers.common['Authorization'] = token;
     checkLogin();
   }, [])
